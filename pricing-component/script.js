4c7c3b51d2ffe0154de91816faace8696b47b59a
Vinh Hiển <hienvh00@gmail.com>
2020-06-02 16:57:09 +0700

    adada

--- pricing-component/script.js
+++ pricing-component/script.js
@@ -1,7 +1,7 @@
 document.getElementById('switch').onchange = function () {
     let block = document.getElementsByClassName('d-block');
     let none = document.getElementsByClassName('d-none');
-    if (this.checked) {
+    if (!this.checked) {
         for (let i = 0; i < block.length; i++) {
             block[i].style.display = "none";
             none[i].style.display = "block";
